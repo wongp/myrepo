bcf7e7ff5402255f41ba63fa8ab62817d8abe0dc
Ubuntu <ubuntu@ip-172-31-36-177.us-west-2.compute.internal>
2013-08-02 00:32:39 +0000

    renamed nodejssample and updated heroku

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    var buffer = fs.readFileSync("index.html");
+    response.send(buffer.toString()); 
+//    response.send("hi world");
 });
 
 var port = process.env.PORT || 5000;
