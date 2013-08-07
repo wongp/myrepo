8e8d481af80f97ec16db8ed7d8128ee0bf50650c
Ubuntu <ubuntu@ip-172-31-41-4.us-west-2.compute.internal>
2013-08-07 23:41:35 +0000

    modified files, only index has proper js and css files

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-app.use(express.logger());
+//app.use(express.logger());
+
+app.use("/", express.static(__dirname + '/'));
 
 app.get('/', function(request, response) {
     var buffer = fs.readFileSync("index.html");
     response.send(buffer.toString()); 
 //    response.send("hi world");
+
+//    buffer = fs.readFileSync("contacts.html");
+//    response.send(buffer.toString());
 });
+
+app.get('/contacts.html', function(req, res) {
+    var buffer = fs.readFileSync("contacts.html");
+    res.send(buffer.toString());
+});
+
+app.get('/courses.html', function(req, res) {
+    var buffer = fs.readFileSync("courses.html");
+    res.send(buffer.toString());
+});
+
+app.get('/locationhours.html', function(req, res) {
+    var buffer = fs.readFileSync("locationhours.html");
+    res.send(buffer.toString());
+});
+
+
 
 var port = process.env.PORT || 8080;
 console.log("begin listening....");
